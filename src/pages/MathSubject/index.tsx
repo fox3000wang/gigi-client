7545a321bb6afb354fe42cc40e9a2cd6d5374f84
fox.wang <fox.wang@saybot.com>
2022-04-21 09:21:15 +0800

    生成加法减法

--- src/pages/MathSubject/index.tsx
+++ src/pages/MathSubject/index.tsx
@@ -22,14 +22,49 @@ function MathSubject(props: any) {
     );
   }
 
-  const mathSubject: any[] = [];
-  for (let i: any = 0; i < 104; i++) {
-    mathSubject.push([
-      Math.floor(Math.random() * config.level),
-      Math.floor(Math.random() * config.level),
-    ]);
+  function randomNum(min: number, max: number) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
   }
 
+  // 生成 0-9 之间的随机数
+  function gemRandom() {
+    return Math.floor(Math.random() * config.level);
+  }
+
+  // 生成加法题
+  function gemAdd() {
+    const subject: any[] = [];
+    config.operator = ['+'];
+    for (let i: any = 0; i < 104; i++) {
+      let a: any = gemRandom();
+      let b: any = gemRandom();
+      while (a + b > 10) {
+        b = gemRandom();
+      }
+      subject.push([a, b]);
+    }
+    return subject;
+  }
+
+  // 生成减法题
+  function gemSub() {
+    const subject: any[] = [];
+    config.operator = ['-'];
+    for (let i: any = 0; i < 104; i++) {
+      let a: any = gemRandom() + 1;
+      let b: any = gemRandom();
+      while (a - b < 0) {
+        b = gemRandom();
+      }
+      subject.push([a, b]);
+    }
+    return subject;
+  }
+
+  //const mathSubject: any[] = gemAdd();
+  const mathSubject: any[] = gemSub();
+
+  const operatior = config.operator[0];
   return (
     <div className='bg'>
       <Title txt='数学题'>
@@ -37,18 +72,17 @@ function MathSubject(props: any) {
       </Title>
       <div className='scroll-box'>
         <div>
-          <div className='words'>
+          <div className='subjects'>
             {mathSubject.map((e: any, i: number) => {
               return (
                 <div className='mathSubject' key={i}>
-                  {e[0]} + {e[1]} =
+                  {e[0]} {operatior} {e[1]} =
                 </div>
               );
             })}
           </div>
         </div>
       </div>
-      {/* <div className='analysis-buttom'></div> */}
     </div>
   );
 }
